6b8075b06144b7880ddc59df306b681f0414f065
Sriram Bhat <srirambhat7@gmail.com>
2020-09-12 09:26:41 -0500

    Updates

---
+++ precache-manifest.9a0b0e491a381a8c3249dcd0e55d6a4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e75e190d65c8f2d1e958d0ad8b8ffc73",
+    "url": "/quote-guesser/index.html"
+  },
+  {
+    "revision": "bf5854f4fdfc7274d005",
+    "url": "/quote-guesser/static/css/main.0741bc4a.chunk.css"
+  },
+  {
+    "revision": "8cdc868e6eb919b56a72",
+    "url": "/quote-guesser/static/js/2.06314353.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-guesser/static/js/2.06314353.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5854f4fdfc7274d005",
+    "url": "/quote-guesser/static/js/main.600ebc80.chunk.js"
+  },
+  {
+    "revision": "be8aec69e02d7294963b",
+    "url": "/quote-guesser/static/js/runtime-main.22a2e29a.js"
+  }
+]);
